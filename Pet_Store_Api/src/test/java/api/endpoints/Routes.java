63eda5d4a8975eae90c29c8c56b2770da1de4d24
lakshmi1687 <84918978+lakshmi1687@users.noreply.github.com>
2023-05-13 21:08:00 +0530

    Add files via upload

---
+++ Pet_Store_Api/src/test/java/api/endpoints/Routes.java
@@ -0,0 +1,14 @@
+package api.endpoints;
+
+// add all the urls which required to test
+
+public class Routes {
+	
+	public static String baseUrl = "https://petstore.swagger.io/v2";
+	public static String postUrl = baseUrl +"/user";
+	public static String getUrl = baseUrl + "/user/{username}";
+	
+	
+	
+
+}
